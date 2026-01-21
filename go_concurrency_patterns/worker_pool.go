e94176aa608dc9836240c899b26513e52330422a
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-01-21 18:51:35 +0530

    worker pool pattern

---
+++ go_concurrency_patterns/worker_pool.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Site struct {
+	url string
+}
+
+type Result struct {
+	url        string
+	statusCode int
+}
+
+func crawl(wid int, jobs <-chan Site, results chan<- Result) {
+	for site := range jobs {
+		fmt.Println("worker id: ", wid)
+
+		resp, err := http.Get(site.url)
+		if err != nil {
+			log.Println(err.Error())
+		}
+		resp.Body.Close()
+		results <- Result{
+			url:        site.url,
+			statusCode: resp.StatusCode,
+		}
+	}
+}
+
+func main() {
+	jobs := make(chan Site, 3)
+	result := make(chan Result, 3)
+
+	for w := 1; w <= 3; w++ {
+		go crawl(w, jobs, result)
+	}
+	fmt.Println("worker pool initialized")
+
+	time.Sleep(3 * time.Second)
+
+	urls := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.instagram.com",
+		"https://tutorialedge.net/pricing/",
+		"https://www.reddit.com",
+	}
+	for _, url := range urls {
+		jobs <- Site{url: url}
+	}
+	close(jobs)
+
+	time.Sleep(3 * time.Second)
+	for i := 0; i < 5; i++ {
+		r := <-result
+		fmt.Printf("%+v\n", r)
+	}
+}
